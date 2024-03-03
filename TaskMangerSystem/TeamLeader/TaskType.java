cc3d4154e72dd218cae91a76538279eff6aaeda6
abedalrahman alkayyali <202110142@stu.meu.edu.jo>
2024-03-03 23:31:32 +0300

    add the Package that need for the project for begaining

---
+++ TaskMangerSystem/TeamLeader/TaskType.java
@@ -0,0 +1,41 @@
+public class TaskType { 
+
+  private String TaskName;
+  private String TaskDiscribtion;
+  private static int TaskID = 0;
+  private int Taskid;
+
+
+  // this function is used to set the taskname which given as string parameter 
+  public void setTaskName(String TaskName) {
+    this.TaskName = TaskName;
+  }
+
+  // this function is used to ge the taskname value its constant no edite for value 
+  public final String getTaskName() {
+    return TaskName;
+  }
+
+  // this function is used to se the TaskDiscribtion for the task as parameter 
+  public void setTaskDiscribtion(String TaskDiscribtion) {
+    this.TaskDiscribtion = TaskDiscribtion;
+  }
+
+  // this function is uses to get the TaskDiscribtion value constant to edite for value 
+  public final String getTaskDiscribtion() {
+    return TaskDiscribtion;
+  }
+
+  // this function is used to get the task id 
+  public final int getTaskID() {
+    return Taskid;
+  }
+
+  // default constructer 
+  public TaskType() {
+    this.TaskName = " ";
+    this.TaskDiscribtion = " ";
+    this.Taskid = TaskType.TaskID;
+    TaskType.TaskID++;
+  }
+}
